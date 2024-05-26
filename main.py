f6af8901010c94cc3d6924f2f1451cfc1e81c989
rohk30 <134207791+rohk30@users.noreply.github.com>
2024-05-26 22:26:56 +0530

    Final Commit

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-from keras.models import load_model
+print("Start")
+
+
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 from time import sleep
-from keras.preprocessing.image import img_to_array
-from keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import img_to_array
 import cv2
 import numpy as np
+import time
 
-face_classifier = cv2.CascadeClassifier(r'C:\Users\Admin\Desktop\PythonProject\EmotionDetectionCNN\haarcascade_frontalface_default.xml')
-classifier =load_model(r'C:\Users\Admin\Desktop\PythonProject\EmotionDetectionCNN\model.h5')
+print("Working??")
 
-emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
+face_classifier = cv2.CascadeClassifier(r"C:\Users\Rohii\Desktop\PythonForML\FaceEmotionDetection\haarcascade_frontalface_default.xml")
+classifier = load_model(r"C:\Users\Rohii\Desktop\PythonForML\FaceEmotionDetection\model.h5")
+
+# emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
+emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
 
 cap = cv2.VideoCapture(0)
-
-
 
 while True:
     _, frame = cap.read()
@@ -25,16 +30,17 @@ while True:
         roi_gray = gray[y:y+h,x:x+w]
         roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
 
-
-
         if np.sum([roi_gray])!=0:
             roi = roi_gray.astype('float')/255.0
             roi = img_to_array(roi)
             roi = np.expand_dims(roi,axis=0)
 
             prediction = classifier.predict(roi)[0]
+            print(prediction)
+            #time.sleep(2)
             label=emotion_labels[prediction.argmax()]
             label_position = (x,y)
+            print(label)
             cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
         else:
             cv2.putText(frame,'No Faces',(30,80),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
